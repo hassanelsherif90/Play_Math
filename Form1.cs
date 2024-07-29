052f9d80789853bdfe7691fb1f4a5a61cce89445
Hassan El Sherif <hassanelsherif90@gmail.com>
2024-07-29 15:56:52 +0300

    Update

--- Form1.cs
+++ Form1.cs
@@ -1,67 +1,101 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Play_Math.Quizz;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using static Play_Math.Quizz.FrmQuizz;
-
 
 namespace Play_Math
 {
     public partial class Form1 : Form
     {
+        private QuizSettings _quizSettings;
+
         public Form1()
         {
             InitializeComponent();
-        }
-        public QuestionLevel Level { get; set; }
-        public OperationType OpType { get; set; }
-        public int NumberOfQuestions { get; set; }
-
-        private void btnStartQuize_Click(object sender, EventArgs e)
-        {
-            Form frm = new FrmQuizz(Level, OpType);
-            frm.ShowDialog();
-        }
-
-        private void FillQuizzLevel()
-        {
-            CBQuize_Level.DataSource = Enum.GetValues(typeof(QuestionLevel));
-        }
-
-        private void FillQuestionsLevel()
-        {
-            CBOperation_Type.DataSource = Enum.GetValues(typeof(OperationType));
-        }
-
-        private void CBQuize_Level_SelectedIndexChanged(object sender, EventArgs e)
-        {
-
-            Level = (QuestionLevel)CBQuize_Level.SelectedItem;
+            _quizSettings = new QuizSettings();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            FillQuestionsLevel();
-            FillQuizzLevel();
+            InitializeControls();
         }
 
-        private void Nu_NumberQuestion_ValueChanged(object sender, EventArgs e)
+        private void InitializeControls()
         {
-            NumberOfQuestions = (int)Nu_NumberQuestion.Value;
+            InitializeComboBoxes();
+            InitializeNumberOfQuestions();
+            UpdateStartButtonState();
+        }
+
+        private void InitializeComboBoxes()
+        {
+            InitializeComboBox(CBQuize_Level, typeof(QuestionLevel));
+            InitializeComboBox(CBOperation_Type, typeof(OperationType));
+        }
+
+        private void InitializeComboBox(ComboBox comboBox, Type enumType)
+        {
+            comboBox.DataSource = Enum.GetValues(enumType);
+            comboBox.SelectedIndex = 0;
+        }
+
+        private void InitializeNumberOfQuestions()
+        {
+            Nu_NumberQuestion.Minimum = 1;
+            Nu_NumberQuestion.Maximum = 10;
+            Nu_NumberQuestion.Value = 10; // Default value
+            _quizSettings.NumberOfQuestions = (int)Nu_NumberQuestion.Value;
+        }
+
+        private void UpdateStartButtonState()
+        {
+            btnStartQuize.Enabled = _quizSettings.IsValid();
+        }
+
+        private void btnStartQuize_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (FrmQuizz  quizForm = new FrmQuizz(_quizSettings.Level, _quizSettings.OpType, _quizSettings.NumberOfQuestions))
+                {
+                    quizForm.ShowDialog(this);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CBQuize_Level_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _quizSettings.Level = (QuestionLevel)CBQuize_Level.SelectedItem;
+            UpdateStartButtonState();
         }
 
         private void CBOperation_Type_SelectedIndexChanged(object sender, EventArgs e)
         {
-            OpType = (OperationType)CBOperation_Type.SelectedItem;
+            _quizSettings.OpType = (OperationType)CBOperation_Type.SelectedItem;
+            UpdateStartButtonState();
         }
 
+        private void Nu_NumberQuestion_ValueChanged(object sender, EventArgs e)
+        {
+            _quizSettings.NumberOfQuestions = (int)Nu_NumberQuestion.Value;
+            UpdateStartButtonState();
+        }
+    }
 
+    public class QuizSettings
+    {
+        public QuestionLevel Level { get; set; }
+        public OperationType OpType { get; set; }
+        public int NumberOfQuestions { get; set; }
+
+        public bool IsValid()
+        {
+            return Enum.IsDefined(typeof(QuestionLevel), Level) &&
+                   Enum.IsDefined(typeof(OperationType), OpType) &&
+                   NumberOfQuestions > 0 && NumberOfQuestions <= 10;
+        }
     }
 }
--- Quizz/FrmQuizz.cs
+++ Quizz/FrmQuizz.cs
@@ -1,7 +1,8 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Windows.Forms;
+using System.Threading.Tasks;
 
 namespace Play_Math.Quizz
 {
@@ -9,65 +10,116 @@ namespace Play_Math.Quizz
     {
         private readonly Random _rand = new Random();
 
-        private int correctAnswer; // Store the correct answer for comparison
+        private readonly Quiz _currentQuiz;
 
-        public FrmQuizz()
+        private int _currentQuestionIndex;
+
+        public FrmQuizz(QuestionLevel level, OperationType opType, int numberOfQuestions)
         {
             InitializeComponent();
-            InitializeButtonEventHandlers(); // Initialize button event handlers
+            _currentQuiz = new Quiz(level, opType, numberOfQuestions);
+            InitializeButtonEventHandlers();
         }
 
-        public FrmQuizz(QuestionLevel Level, OperationType OpType)
+        private void InitializeButtonEventHandlers()
         {
-            InitializeComponent();
-            this.OpType = OpType;
-            this.Level = Level;
-            InitializeButtonEventHandlers(); // Initialize button event handlers
+            btnResult1.Click += CheckAnswer;
+            btnResult2.Click += CheckAnswer;
+            btnResult3.Click += CheckAnswer;
+            btnResult4.Click += CheckAnswer;
         }
 
-        public enum QuestionLevel
+        private void FrmQuizz_Load(object sender, EventArgs e)
         {
-            Easy = 1,
-            Medium,
-            Hard,
-            Mixed
+            StartQuiz();
         }
 
-        public enum OperationType
+        private void StartQuiz()
         {
-            Addition = 1,
-            Subtraction,
-            Multiplication,
-            Division,
-            Mixed
+            _currentQuestionIndex = 0;
+            _currentQuiz.GenerateQuestions();
+            DisplayNextQuestion();
         }
 
-        public OperationType OpType { get; set; }
-        public QuestionLevel Level { get; set; }
-
-        public class Question
+        private void DisplayNextQuestion()
         {
-            public int Number1 { get; set; }
-            public int Number2 { get; set; }
-            public OperationType OpType { get; set; }
-            public QuestionLevel Level { get; set; }
-            public int CorrectAnswer { get; set; }
-            public int PlayerAnswer { get; set; }
-            public bool IsCorrect => PlayerAnswer == CorrectAnswer;
+            if (_currentQuestionIndex < _currentQuiz.Questions.Count)
+            {
+                var question = _currentQuiz.Questions[_currentQuestionIndex];
+                DisplayQuestion(question);
+                GenerateAnswerOptions(question.CorrectAnswer);
+            }
+            else
+            {
+                ShowQuizResults();
+            }
         }
 
-        public class Quiz
+        private void DisplayQuestion(Question question)
         {
-            public List<Question> Questions { get; set; } = new List<Question>();
-            public int NumberOfQuestions { get; set; }
-            public OperationType OpType { get; set; }
-            public QuestionLevel Level { get; set; }
-            public int CorrectAnswers { get; set; }
-            public int IncorrectAnswers { get; set; }
-            public bool IsPass => CorrectAnswers >= IncorrectAnswers;
+            lblQuestion1.Text = question.Number1.ToString();
+            lblQuestion2.Text = question.Number2.ToString();
+            lblOperation1.Text = GetOperationSymbol(question.OpType);
         }
 
-        private int GenerateRandomNumber(int from, int to) => _rand.Next(from, to);
+        private void GenerateAnswerOptions(int correctAnswer)
+        {
+            var answers = new List<int> { correctAnswer };
+            while (answers.Count < 4)
+            {
+                int wrongAnswer = GenerateWrongAnswer(correctAnswer);
+                if (!answers.Contains(wrongAnswer))
+                {
+                    answers.Add(wrongAnswer);
+                }
+            }
+
+            answers.Shuffle();
+
+            btnResult1.Text = answers[0].ToString();
+            btnResult2.Text = answers[1].ToString();
+            btnResult3.Text = answers[2].ToString();
+            btnResult4.Text = answers[3].ToString();
+        }
+
+        private void CheckAnswer(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            int chosenAnswer = int.Parse(button.Text);
+            var currentQuestion = _currentQuiz.Questions[_currentQuestionIndex];
+
+            if (chosenAnswer == currentQuestion.CorrectAnswer)
+            {
+                button.BackColor = Color.Green;
+                _currentQuiz.CorrectAnswers++;
+            }
+            else
+            {
+                button.BackColor = Color.Red;
+                _currentQuiz.IncorrectAnswers++;
+            }
+
+            _currentQuestionIndex++;
+            Task.Delay(1000).ContinueWith(_ =>
+            {
+                ResetButtonColors();
+                DisplayNextQuestion();
+            }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private void ResetButtonColors()
+        {
+            btnResult1.BackColor = SystemColors.Control;
+            btnResult2.BackColor = SystemColors.Control;
+            btnResult3.BackColor = SystemColors.Control;
+            btnResult4.BackColor = SystemColors.Control;
+        }
+
+        private void ShowQuizResults()
+        {
+            MessageBox.Show($"Quiz completed!\nCorrect Answers: {_currentQuiz.CorrectAnswers}\nIncorrect Answers: {_currentQuiz.IncorrectAnswers}");
+            Close();
+        }
 
         private string GetOperationSymbol(OperationType opType) =>
             opType switch
@@ -79,123 +131,125 @@ namespace Play_Math.Quizz
                 _ => "?"
             };
 
-        private Question GenerateQuestion(OperationType opType, QuestionLevel level)
+        private int GenerateWrongAnswer(int correctAnswer)
         {
-            if (level == QuestionLevel.Mixed) level = (QuestionLevel)GenerateRandomNumber(1, 3);
-            if (opType == OperationType.Mixed) opType = (OperationType)GenerateRandomNumber(1, 4);
-
-            int number1 = level switch
+            int wrongAnswer;
+            do
             {
-                QuestionLevel.Easy => GenerateRandomNumber(1, 10),
-                QuestionLevel.Medium => GenerateRandomNumber(11, 30),
-                QuestionLevel.Hard => GenerateRandomNumber(31, 100),
-                _ => 1
+                wrongAnswer = correctAnswer + _rand.Next(-10, 11);
+            } while (wrongAnswer == correctAnswer);
+            return wrongAnswer;
+        }
+    }
+
+    public enum QuestionLevel
+    {
+        Easy = 1,
+        Medium,
+        Hard,
+        Mixed
+    }
+
+    public enum OperationType
+    {
+        Addition = 1,
+        Subtraction,
+        Multiplication,
+        Division,
+        Mixed
+    }
+
+    public class Question
+    {
+        public int Number1 { get; }
+        public int Number2 { get; }
+        public OperationType OpType { get; }
+        public QuestionLevel Level { get; }
+        public int CorrectAnswer { get; }
+
+        public Question(int number1, int number2, OperationType opType, QuestionLevel level, int correctAnswer)
+        {
+            Number1 = number1;
+            Number2 = number2;
+            OpType = opType;
+            Level = level;
+            CorrectAnswer = correctAnswer;
+        }
+    }
+
+    public class Quiz
+    {
+        private readonly Random _rand = new Random();
+        public List<Question> Questions { get; } = new List<Question>();
+        public QuestionLevel Level { get; }
+        public OperationType OpType { get; }
+        public int NumberOfQuestions { get; }
+        public int CorrectAnswers { get; set; }
+        public int IncorrectAnswers { get; set; }
+        public bool IsPass => CorrectAnswers >= IncorrectAnswers;
+
+        public Quiz(QuestionLevel level, OperationType opType, int numberOfQuestions)
+        {
+            Level = level;
+            OpType = opType;
+            NumberOfQuestions = numberOfQuestions;
+        }
+
+        public void GenerateQuestions()
+        {
+            Questions.Clear();
+            for (int i = 0; i < NumberOfQuestions; i++)
+            {
+                Questions.Add(GenerateQuestion());
+            }
+        }
+
+        private Question GenerateQuestion()
+        {
+            var level = Level == QuestionLevel.Mixed ? (QuestionLevel)_rand.Next(1, 4) : Level;
+            var opType = OpType == OperationType.Mixed ? (OperationType)_rand.Next(1, 5) : OpType;
+
+            var (number1, number2) = GenerateNumbers(level);
+            int correctAnswer = CalculateAnswer(number1, number2, opType);
+
+            return new Question(number1, number2, opType, level, correctAnswer);
+        }
+
+        private (int, int) GenerateNumbers(QuestionLevel level) =>
+            level switch
+            {
+                QuestionLevel.Easy => (_rand.Next(1, 10), _rand.Next(1, 10)),
+                QuestionLevel.Medium => (_rand.Next(11, 30), _rand.Next(11, 30)),
+                QuestionLevel.Hard => (_rand.Next(31, 100), _rand.Next(31, 100)),
+                _ => throw new ArgumentOutOfRangeException(nameof(level))
             };
 
-            int number2 = level switch
-            {
-                QuestionLevel.Easy => GenerateRandomNumber(1, 10),
-                QuestionLevel.Medium => GenerateRandomNumber(11, 30),
-                QuestionLevel.Hard => GenerateRandomNumber(31, 100),
-                _ => 1
-            };
-
-            int correctAnswer = opType switch
+        private int CalculateAnswer(int number1, int number2, OperationType opType) =>
+            opType switch
             {
                 OperationType.Addition => number1 + number2,
                 OperationType.Subtraction => number1 - number2,
                 OperationType.Multiplication => number1 * number2,
                 OperationType.Division => number1 / number2,
-                _ => 0
+                _ => throw new ArgumentOutOfRangeException(nameof(opType))
             };
+    }
 
-            return new Question
+    public static class ListExtensions
+    {
+        private static Random _rand = new Random();
+
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
             {
-                Number1 = number1,
-                Number2 = number2,
-                OpType = opType,
-                Level = level,
-                CorrectAnswer = correctAnswer
-            };
-        }
-
-        private void FrmQuizz_Load(object sender, EventArgs e)
-        {
-            FillQuestions();
-        }
-
-        private void FillQuestions()
-        {
-            var qu = GenerateQuestion(OpType, Level);
-            lblQuestion1.Text = qu.Number1.ToString();
-            lblQuestion2.Text = qu.Number2.ToString();
-            lblOperation1.Text = GetOperationSymbol(qu.OpType);
-
-            correctAnswer = qu.CorrectAnswer; // Store the correct answer
-
-            GenerateAnswers(correctAnswer, 4);
-        }
-
-        private void GenerateAnswers(int correctAnswer, int numOfAnswers)
-        {
-            List<int> answers = new List<int> { correctAnswer };
-
-            while (answers.Count < numOfAnswers)
-            {
-                int wrongAnswer = _rand.Next(correctAnswer - 10, correctAnswer + 10);
-
-                if (!answers.Contains(wrongAnswer) && wrongAnswer != correctAnswer)
-                {
-                    answers.Add(wrongAnswer);
-                }
-            }
-
-            answers = ShuffleAnswers(answers);
-
-            btnResult1.Text = answers[0].ToString();
-            btnResult2.Text = answers[1].ToString();
-            btnResult3.Text = answers[2].ToString();
-            btnResult4.Text = answers[3].ToString();
-        }
-
-        private List<int> ShuffleAnswers(List<int> answers)
-        {
-            for (int i = 0; i < answers.Count; i++)
-            {
-                int randomIndex = _rand.Next(i, answers.Count);
-                int temp = answers[i];
-                answers[i] = answers[randomIndex];
-                answers[randomIndex] = temp;
-            }
-
-            return answers;
-        }
-
-        private void InitializeButtonEventHandlers()
-        {
-            btnResult1.Click += CheckAnswer;
-            btnResult2.Click += CheckAnswer;
-            btnResult3.Click += CheckAnswer;
-            btnResult4.Click += CheckAnswer;
-        }
-
-        private void CheckAnswer(object sender, EventArgs e)
-        {
-            Button clickedButton = sender as Button;
-
-            int chosenAnswer = int.Parse(clickedButton.Text);
-
-            if (chosenAnswer == correctAnswer)
-            {
-                //MessageBox.Show("Correct :-)", "Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                clickedButton.BackColor = System.Drawing.Color.Green;
-            }
-            else
-            {
-                //MessageBox.Show($"Incorrect :-( Correct Answer: {correctAnswer}", "Result", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                clickedButton.BackColor = System.Drawing.Color.Red;
+                n--;
+                int k = _rand.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
             }
         }
-
     }
 }
